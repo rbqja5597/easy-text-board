95be1343ccaa6bb5b7596e6dbf533eac3e189c1a
rbqja5597 <rbqja5597@naver.com>
2024-10-24 21:09:17 +0900

    String  조건문 사용

--- src/easy_text_board/Main.java
+++ src/easy_text_board/Main.java
@@ -2,7 +2,14 @@ package easy_text_board;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.print("안녕");
+		String command = "artcle add";
+		
+		if (command.equals("artcle add") ) {
+			System.out.println("== 게시물 등록 ==");
+		}
+		if (command.equals("artcle list") ) {
+			System.out.println("== 게시물 리스트 ==");
+		}
 	}
 }
 	
