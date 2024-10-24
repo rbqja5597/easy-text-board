85536a1f557e843fbf93b77d4ca269f908fb37a1
rbqja5597 <rbqja5597@naver.com>
2024-10-24 21:32:08 +0900

    while 무한반복 고객이 종료를 원할때 까지 실행되도록

--- src/easy_text_board/Main.java
+++ src/easy_text_board/Main.java
@@ -6,6 +6,8 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
+		
+		while (true) {
 		System.out.printf("명령어) ");
 		String command = scanner.nextLine();
 		
@@ -13,11 +15,15 @@ public class Main {
 			System.out.println("== 게시물 등록 ==");
 		} else if (command.equals("artcle list")) {
 			System.out.println("== 게시물 리스트 ==");
+		} else if (command.equals("system exit") ) {
+			System.out.println("== 프로그램 종료 ==");
+			break;
 		} else {
 			System.out.println("== 존재하지 않는 명령어 ==");
 		}
-		
+	}
 		scanner.close();
 	}
+
 }
 	
