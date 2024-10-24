12b461c9cdc574b8574c5be089fd24e610af6483
rbqja5597 <rbqja5597@naver.com>
2024-10-24 21:21:25 +0900

    스캐너로 고객에게 명령어 입력받기

--- src/easy_text_board/Main.java
+++ src/easy_text_board/Main.java
@@ -1,15 +1,23 @@
 package easy_text_board;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		String command = "artcle add";
+		Scanner scanner = new Scanner(System.in);
 		
-		if (command.equals("artcle add") ) {
+		System.out.printf("명령어) ");
+		String command = scanner.nextLine();
+		
+		if (command.equals("artcle add")) {
 			System.out.println("== 게시물 등록 ==");
-		}
-		if (command.equals("artcle list") ) {
+		} else if (command.equals("artcle list")) {
 			System.out.println("== 게시물 리스트 ==");
+		} else {
+			System.out.println("== 존재하지 않는 명령어 ==");
 		}
+		
+		scanner.close();
 	}
 }
 	
